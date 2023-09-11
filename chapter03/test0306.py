44867ed6ad30d3e3e8cf9907803494884b91a28a
pokabu55 <okazumaru@gmail.com>
2023-09-11 17:41:13 +0900

    端数計算を別途やってみた（完了）

--- chapter03/test0306.py
+++ chapter03/test0306.py
@@ -6,17 +6,23 @@ layout = [
     [sg.T("金額と人数を入力してください")],
     [sg.T("金額"), sg.I("1000", k="totalCost")],
     [sg.T("人数"), sg.I("4", k="number")],
-    [sg.B("実行", k="calcBtn"), sg.T("答え", k="outputTxt")],
+    [sg.B("実行", k="calcBtn")],
+    [sg.T("答え", k="outputTxt")],
 ]
 
-win = sg.Window("割り勘アプリ", layout, font=(None, 14), size=(400, 300))
+win = sg.Window("割り勘アプリ", layout, font=(None, 14), size=(500, 300))
 
 
 def execute():
     print("test")
     cost = int(v["totalCost"])
     num = int(v["number"])
-    txt = f"１人あたりの金額は、{cost/num:.2f}円です。"
+
+    # 端数を別途計算したい
+    payment = int(cost / num)
+    fraction = cost - payment * num
+
+    txt = f"１人あたりの金額は{payment:.0f}円で、端数は{fraction:.0f}円です。"
     win["outputTxt"].update(txt)
 
 
